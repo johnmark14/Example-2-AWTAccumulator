0c8062ed2367888119e4a6457d9da8d2cdab8df6
johnmark14 <johmarkgabrentina@gmail.com>
2021-05-11 11:03:35 +0800

    Init

---
+++ src/main/AWTAccumulator.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author John Mark Gabrentina
+ *
+ */
+public class AWTAccumulator {
+
+	/**
+	 * 
+	 */
+	public AWTAccumulator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
